ae1a58de865d297dcc7ab79268d477082c391945
dmitryhtsnk <116877726+dmitryhtsnk@users.noreply.github.com>
2024-01-11 01:46:33 +0200

    Product display
    Implemented the display of products on the page using the render method

---
+++ components/Products/Products.js
@@ -0,0 +1,25 @@
+class Products{
+
+    render(){
+        let htmlCatalog = '';
+
+        CATALOG.forEach(({id, name, price, img}) => {
+            htmlCatalog += `
+                <li>
+                    <span>${name}</span>
+                    <img src='${img}'>
+                    <span>${price}</span>
+                    <button>Додати в кошик</button>
+                </li>
+            `;
+        });
+        const html = `
+            <ul>${htmlCatalog}</ul>
+        `;
+
+        ROOT_PRODUCTS.innerHTML = html;
+    }
+}
+    
+const productsPage = new Products();
+productsPage.render();
