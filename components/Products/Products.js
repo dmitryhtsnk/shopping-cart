3d19d08c7a181dcfdd6e4918e3b29a80f9e550d1
dmitryhtsnk <116877726+dmitryhtsnk@users.noreply.github.com>
2024-01-11 19:48:53 +0200

    Design
    Product catalog styling

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -1,25 +1,24 @@
-class Products{
+class Products {
+  render() {
+    let htmlCatalog = "";
 
-    render(){
-        let htmlCatalog = '';
-
-        CATALOG.forEach(({id, name, price, img}) => {
-            htmlCatalog += `
-                <li>
-                    <span>${name}</span>
-                    <img src='${img}'>
-                    <span>${price}</span>
-                    <button>Додати в кошик</button>
+    CATALOG.forEach(({ id, name, price, img }) => {
+      htmlCatalog += `
+                <li class="products-element">
+                    <span class="products-element__name">${name}</span>
+                    <img class="products-element__img" src='${img}'>
+                    <span class="products-element__price">⚡️ ${price.toLocaleString()} UAH</span>
+                    <button class="products-element__btn">Додати в кошик</button>
                 </li>
             `;
-        });
-        const html = `
-            <ul>${htmlCatalog}</ul>
+    });
+    const html = `
+            <ul class="products-container">${htmlCatalog}</ul>
         `;
 
-        ROOT_PRODUCTS.innerHTML = html;
-    }
+    ROOT_PRODUCTS.innerHTML = html;
+  }
 }
-    
+
 const productsPage = new Products();
 productsPage.render();
